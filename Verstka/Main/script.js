a788d636016be23f2fc76c15e481f7666572206c
Denis18UKS <honorxpremium75@gmail.com>
2023-12-01 01:42:23 +0500

    upd_files

--- Verstka/Main/script.js
+++ Verstka/Main/script.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     let modalButton = document.querySelector('.reg');
     
 });
+
+function checkRegUser(){
+    alert("Сначала зарегистрируйтесь или войдите в аккаунт")
+}
