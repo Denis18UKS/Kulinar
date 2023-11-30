c67efebc0d532bbe8f3f4e78104d70d224e8ad0e
qwe <honorxpremium75@gmail.com>
2023-11-30 19:22:44 +0500

    updates_shops

--- Verstka/Shop-cart/script.js
+++ Verstka/Shop-cart/script.js
@@ -1,3 +1,4 @@
+
 let amount = Array.from(document.getElementsByClassName('number'));
 let onePrice = document.getElementsByClassName('oneprice');
 let morePrice = document.getElementsByClassName('moreprice');
@@ -6,40 +7,32 @@ let parent = document.getElementById('items');
 let child = document.getElementsByClassName('item');
 let sum = 0;
 
-
-
-
-amount.forEach(
-    (el, key) => {
+amount.forEach((el, key) => {
     console.log(morePrice[key].textContent);
     sum += +morePrice[key].textContent;
     console.log(sum);
-    document.getElementById('sum').textContent = sum;
+    document.getElementById('sum').textContent = sum + '₽';
 
     document.getElementsByClassName('minus')[key].addEventListener('click', function() {
-        sum -= +morePrice[key].textContent;
+        sum -= +onePrice[key].textContent;
+        document.getElementById('sum').textContent = sum + '₽';  
 
         amount[key].textContent = +(amount[key].textContent) - 1;
         morePrice[key].textContent = +(amount[key].textContent) * +(onePrice[key].textContent);
 
         if(+(amount[key].textContent) <= 0) {
-            items.removeChild(child[key]);
+            parent.removeChild(child[key]);
         }
-        sum += +morePrice[key].textContent;
-        document.getElementById('sum').textContent = sum;
     });
-    
+
     document.getElementsByClassName('plus')[key].addEventListener('click', function() {
-        sum -= +morePrice[key].textContent;
         if (amount[key].textContent == 9) {
             return;
         }
         amount[key].textContent = +(amount[key].textContent) + 1;
         morePrice[key].textContent = +(amount[key].textContent) * +(onePrice[key].textContent);
 
-        sum += +morePrice[key].textContent;
-        document.getElementById('sum').textContent = sum;
+        sum += +onePrice[key].textContent;
+        document.getElementById('sum').textContent = sum + '₽';
     });
 });
-
-
