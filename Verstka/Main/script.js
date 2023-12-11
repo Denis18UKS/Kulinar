5cbf98f5e77bada7c59310b7a0a383c400a4c11b
qwe <honorxpremium75@gmail.com>
2023-12-11 14:07:09 +0500

    justupdate

--- Verstka/Main/script.js
+++ Verstka/Main/script.js
@@ -1,10 +1,3 @@
-Array.from(document.getElementsByClassName('item__btn')).forEach(el => {
-    el.addEventListener('click', function() {
-        alert('Товар добавлен в корзину');
-    });
-}); 
-
-
 function error(){
     alert("Вы не зарегистрированы!")
 }
