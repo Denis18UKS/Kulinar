20ae9ebccb505303a78fa2146a0a5b68eadc0e86
Denis18UKS <honorxpremium75@gmail.com>
2023-12-11 21:54:06 +0500

    saveCHANGEcodeON75%

--- Verstka/MAINS/Main/script.js
+++ Verstka/MAINS/Main/script.js
@@ -1,11 +1,4 @@
-function error(){
-    alert("Вы не зарегистрированы!")
-}
-<<<<<<< HEAD
-
 let itemBtns = document.querySelectorAll('.item__btn');
 itemBtns.forEach((el) => {
     el.addEventListener('click', error);
 });
-=======
->>>>>>> 5cbf98f5e77bada7c59310b7a0a383c400a4c11b
